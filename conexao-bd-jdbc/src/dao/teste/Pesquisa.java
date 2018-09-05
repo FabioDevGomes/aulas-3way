5f2a3e564688bfe64b2596549588bffa7f501164
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-09-04 23:35:10 -0300

    refatoração simples

--- conexao-bd-jdbc/src/dao/teste/Pesquisa.java
+++ conexao-bd-jdbc/src/dao/teste/Pesquisa.java
@@ -12,39 +12,26 @@ public class Pesquisa {
 //		excecutarDelete(9);
 		System.out.println("---");
 		testarConsulta();
-
-
 	}
-	
+
 	public static void testarUpdate() {
 		Livro livro = new Livro();
 		livro.setCodigo(1);
 		livro.setTitulo("Alterado");
 		livro.setAutor("Fábio Alves");
-		
+
 		dao.updateTitulo(livro);
 	}
-	
+
 	public static void testarConsulta() {
 		String titulo = "a";
-//		LivroDao dao = new LivroDao();
-//		dao.consultar(titulo).forEach(p -> System.out.println("Desc: \t" + p.getTitulo() + " \t\t\tPreço:" + p.getPreco()));
-		
+
 		for (Livro  livro : dao.consultar(titulo)) {
 //			System.out.println(livro.getTitulo());
 			System.out.println("Cod: " + livro.getCodigo() + " \t\tDesc: \t" + livro.getTitulo() + " \t -Preço:" + livro.getPreco());
 		}
-		
-		// deixar usar essa implementação e corrigir o erro
-		// dao.consultar(titulo).forEach(p -> System.out.println("Desc: " +
-		// p.getDescricao() + "Preço:" + p.getPreco()));
-		
-		// implementar usando o for each
-		// dao.consultar(titulo).forEach(p -> System.out.println("Desc: " +
-		// p.getDescricao() + "Preço:" + p.getPreco()));
-		
 	}
-	
+
 	public static void excecutarDelete(int codigo) {
 		dao.removeById(codigo);
 	}
