f3ccc6e9d0f3244dbfc6edbde50dc7aa4b932c2d
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-09-06 18:35:50 -0300

    ajustes

--- conexao-bd-jdbc/src/dao/LivroDao.java
+++ conexao-bd-jdbc/src/dao/LivroDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,17 +14,17 @@ import sql.Sql;
 import util.FabricaConexao;
 
 public class LivroDao {
-	Logger LOG = Logger.getGlobal();
 
-//	private static final String OBTER_POR_ID_SQL = "SELECT AUTOR, TITULO, COD_LIVRO, IMAGEM,PRECO, DESCRICAO FROM ESTOQUE WHERE COD_LIVRO = ?";
-//	private static final String CONSULTAR_SQL = "SELECT COD_LIVRO, TITULO, AUTOR, PRECO, IMAGEM, DESCRICAO FROM ESTOQUE WHERE TITULO LIKE ?";
+	private static final String OBTER_POR_ID_SQL = "SELECT AUTOR, TITULO, COD_LIVRO, IMAGEM,PRECO, DESCRICAO FROM LIVRO WHERE COD_LIVRO = ?";
+	private static final String CONSULTAR_SQL = "SELECT COD_LIVRO, TITULO, AUTOR, PRECO, IMAGEM, DESCRICAO FROM LIVRO WHERE TITULO LIKE ?";
 //	private static final String CONSULTAR_SQL = "SELECT * FROM ESTOQUE WHERE TITULO LIKE ?";
-//	private static final String UPDATE_SQL = "UPDATE ESTOQUE SET TITULO = ?, AUTOR = ? WHERE COD_LIVRO = ?";
+	private static final String UPDATE_SQL = "UPDATE LIVRO SET TITULO = ?, AUTOR = ? WHERE COD_LIVRO = ?";
 
 	public Livro consultar(int codigo) {
 		Livro livro = null;
 		try (Connection conexao = FabricaConexao.getConexao();
-				PreparedStatement consulta = conexao.prepareStatement(Sql.OBTER_POR_ID_SQL.getSql());) {
+				PreparedStatement consulta = conexao.prepareStatement(OBTER_POR_ID_SQL);) {
+//			PreparedStatement consulta = conexao.prepareStatement(Sql.OBTER_POR_ID_SQL.getSql());) {
 			consulta.setInt(1, codigo);
 
 			ResultSet resultado = consulta.executeQuery();
@@ -41,7 +42,6 @@ public class LivroDao {
 			resultado.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			LOG.severe(e.toString());
 		}
 		return livro;
 	}
@@ -49,7 +49,7 @@ public class LivroDao {
 	public List<Livro> consultar(String titulo) {
 		ArrayList<Livro> lista = new ArrayList<Livro>();
 		try (Connection conexao = FabricaConexao.getConexao();
-				PreparedStatement consulta = conexao.prepareStatement(Sql.CONSULTAR_SQL.getSql());) {
+				PreparedStatement consulta = conexao.prepareStatement(CONSULTAR_SQL);) {
 			consulta.setString(1, "%" + titulo.toUpperCase() + "%");
 
 			ResultSet resultado = consulta.executeQuery();
@@ -68,27 +68,22 @@ public class LivroDao {
 			resultado.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			LOG.severe(e.toString());
 		}
 		return lista;
 	}
 
 	public void updateTitulo(Livro livro) {
 
-		// String sql = "UPDATE ESTOQUE SET TITULO = ?, AUTOR = ? " + " WHERE COD_LIVRO
-		// = ?";
-
 		// Cria uma conexão com o banco
 		// Cria um PreparedStatment, classe usada para executar a query
-
 		try (Connection conexao = FabricaConexao.getConexao();
-				PreparedStatement consulta = conexao.prepareStatement(Sql.UPDATE_SQL.getSql());) {
+				PreparedStatement consulta = conexao.prepareStatement(UPDATE_SQL);) {
 
 			// Adiciona o valor do primeiro parâmetro da sql
 			consulta.setString(1, livro.getTitulo());
 			// Adicionar o valor do segundo parâmetro da sql
 			consulta.setString(2, livro.getAutor());
-
+			// Adicionar o valor do terceiro parâmetro da sql
 			consulta.setInt(3, livro.getCodigo());
 
 			// Executa a sql para inserção dos dados
@@ -97,39 +92,44 @@ public class LivroDao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-//		finally {
-//
-//			// Fecha as conexões
-//			try {
-//				if (consulta != null) {
-//					consulta.close();
-//				}
-//
-//				if (conexao != null) {
-//					conexao.close();
-//				}
-//
-//			} catch (Exception e) {
-//
-//				e.printStackTrace();
-//			}
-//		}
 	}
 
 	public void removeById(int id) {
-
 		String sql = "DELETE FROM ESTOQUE WHERE COD_LIVRO = ?";
 
 		try (Connection conexao = FabricaConexao.getConexao();
-				PreparedStatement consulta = conexao.prepareStatement(sql);){
+				PreparedStatement consulta = conexao.prepareStatement(sql);) {
 
 			consulta.setInt(1, id);
-
 			consulta.execute();
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 	}
+
+//	public void save(Contato contato) {
+//		/*
+//		 * Isso é uma sql comum, os ? são os parâmetros que nós vamos adicionar na base
+//		 * de dados
+//		 */
+//		String sql = "INSERT INTO contatos(nome, idade, dataCadastro)" + " VALUES(?,?,?)";
+//
+//		try (Connection conexao = FabricaConexao.getConexao();
+//				PreparedStatement consulta = conexao.prepareStatement(sql);){
+//
+//			// Adiciona o valor do primeiro parâmetro da sql
+//			consulta.setString(1, contato.getNome());
+//			// Adicionar o valor do segundo parâmetro da sql
+//			consulta.setInt(2, contato.getIdade());
+//			// Adiciona o valor do terceiro parâmetro da sql
+//			consulta.setDate(3, new Date(contato.getDataCadastro().getTime()));
+//
+//			// Executa a sql para inserção dos dados
+//			consulta.execute();
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
--- conexao-bd-jdbc/src/dao/teste/Pesquisa.java
+++ conexao-bd-jdbc/src/dao/teste/Pesquisa.java
@@ -8,30 +8,37 @@ public class Pesquisa {
 
 	public static void main(String[] args) {
 		testarConsulta();
-		testarUpdate();
+//		testarUpdate();
 //		excecutarDelete(9);
 		System.out.println("---");
-		testarConsulta();
-	}
+//		testarConsulta();
 
+
+	}
+	
 	public static void testarUpdate() {
+		//passar valores q estão sendo alterados por parâmetro
 		Livro livro = new Livro();
 		livro.setCodigo(1);
 		livro.setTitulo("Alterado");
 		livro.setAutor("Fábio Alves");
-
+		
 		dao.updateTitulo(livro);
 	}
-
+	
 	public static void testarConsulta() {
+		//passar por parâmetro
 		String titulo = "a";
-
+		
 		for (Livro  livro : dao.consultar(titulo)) {
-//			System.out.println(livro.getTitulo());
-			System.out.println("Cod: " + livro.getCodigo() + " \t\tDesc: \t" + livro.getTitulo() + " \t -Preço:" + livro.getPreco());
+			System.out.print(livro.getCodigo()+" ");
+			System.out.print(livro.getTitulo()+" ");
+			System.out.print(livro.getPreco()+" ");
+			System.out.println();
+//			System.out.println("Cod: " + livro.getCodigo() + " \t\tDesc: \t" + livro.getTitulo() + " \t -Preço:" + livro.getPreco());
 		}
 	}
-
+	
 	public static void excecutarDelete(int codigo) {
 		dao.removeById(codigo);
 	}
