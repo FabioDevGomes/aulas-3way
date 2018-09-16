95f845ab9750a88d0fab1decae45848795307a7d
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-09-16 08:32:46 -0300

    projeto com facelets template

---
+++ jpa-jsf-4/src/com/framework/managedbeans/CardLoginManagedBean.java
@@ -0,0 +1,55 @@
+package com.framework.managedbeans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import com.framework.db.UsuarioDAO;
+import com.framework.model.Usuario;
+
+@ManagedBean(name = "cardLoginMB")
+@ViewScoped
+public class CardLoginManagedBean {
+
+	@Inject
+	private UsuarioDAO usuarioDAO;
+	
+	private Usuario usuario = new Usuario();
+
+	public String enviar() {
+
+		apiRecebeDadosCartao();
+
+		usuario = usuarioDAO.getUsuarioCard(usuario.getNomeUsuario(), usuario.getMatricula());
+		if (usuario == null) {
+			usuario = new Usuario();
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Cartão não aceito!", "Erro no Login!"));
+			return null;
+		} else {
+			return "/acessoLiberado";
+		}
+	}
+
+	private void apiRecebeDadosCartao() {
+		String nomeUsuario = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+				.get("nomeUsuario");
+
+		String matricula = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+				.get("matricula");
+
+		usuario.setNomeUsuario(nomeUsuario);
+		usuario.setMatricula(matricula);
+
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
---
+++ jpa-jsf-4/src/com/framework/managedbeans/UsuarioManagedBean.java
@@ -0,0 +1,68 @@
+package com.framework.managedbeans;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import com.framework.db.UsuarioDAO;
+import com.framework.model.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class UsuarioManagedBean {
+
+	private UsuarioDAO usuarioDAO = new UsuarioDAO();
+	private Usuario usuario = new Usuario();
+
+	@PostConstruct
+	public void init() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		String idUsuario = (String) facesContext.getExternalContext().getRequestParameterMap().get("id");
+		if(idUsuario != null) {
+			this.usuario = usuarioDAO.getUsuario(Integer.parseInt(idUsuario));
+		}
+	}
+
+	public List usuarioListDb() {
+		return usuarioDAO.listarUsuario();
+	}
+
+	public void exlcluirUsuarioDb(Usuario usuario) {
+		usuarioDAO.deletarUsuario(usuario);
+	}
+
+	public String incluirUsuarioDb(Usuario usuario) {
+		if (!usuarioDAO.inserirUsuario(usuario)) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro: Usuário já existe!", "--"));
+
+			context.getExternalContext().getFlash().setKeepMessages(true);
+			return "/restrito/novoUsuario.xhtml?faces-redirect=true";
+		}
+		return "/restrito/listagemUsuarioT?faces-redirect=true";
+	}
+
+	public String paginaEditar(Usuario usuario) {
+		this.usuario = usuario;
+		return "/restrito/editarUsuarioT?faces-redirect=true&id="+usuario.getId();
+	}
+
+	public String editarUsuarioDb(Usuario usuario) {
+		usuarioDAO.alterarUsuario();
+		return "/restrito/listagemUsuarioT?faces-redirect=true";
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
