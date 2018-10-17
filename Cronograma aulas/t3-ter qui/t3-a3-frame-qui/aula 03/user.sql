a7959b1118f73d631a20981095961361e45100fb
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-10-17 12:46:34 -0300

    atualizações

---
+++ Cronograma aulas/t3-ter qui/t3-a3-frame-qui/aula 03/user.sql
@@ -0,0 +1,15 @@
+﻿-- Database: "user"
+
+-- DROP DATABASE "user";
+
+CREATE DATABASE "user"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+
+insert into usuario (id, userName, password, lastAccess) values (3, 'admin', '123456', CURRENT_TIMESTAMP);
+insert into usuario (id, userName, password, lastAccess) values (4, 'fabio', '123', CURRENT_TIMESTAMP);
+insert into usuario (id, userName, password, lastAccess) values (5, 'teste', '123', CURRENT_TIMESTAMP);
