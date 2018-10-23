3e31d15cce195ea5877a7cc0b59933b8c8558729
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-10-23 09:13:07 -0300

    atualizações e novo projeto jpa-prime-login

---
+++ jpa-prime-login/src/com/framework/db/UsuarioDAO.java
@@ -0,0 +1,69 @@
+package com.framework.db;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.hibernate.ejb.internal.EntityManagerFactoryRegistry;
+
+import com.framework.model.Usuario;
+
+public class UsuarioDAO {
+
+	private EntityManagerFactory factory = Persistence.createEntityManagerFactory("usuarios");
+	private EntityManager em = factory.createEntityManager();
+	private EntityTransaction transaction = em.getTransaction();
+
+	public Usuario getUsuario(String nomeUsuario, String senha) {
+
+		try {
+			Usuario usuario = (Usuario) em
+					.createQuery("SELECT u from Usuario u where u.nomeUsuario = :name and u.senha = :senha")
+					.setParameter("name", nomeUsuario).setParameter("senha", senha).getSingleResult();
+
+			return usuario;
+		} catch (NoResultException e) {
+			// essa exception não é necessariamente um erro
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public boolean inserirUsuario(Usuario usuario) {
+		if (!transaction.isActive()) {
+			transaction.begin();
+		} 
+		
+		try {
+			em.persist(usuario);
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			// essa exception não é necessariamente um erro
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean deletarUsuario(Usuario usuario) {
+		if (!transaction.isActive()) {
+			transaction.begin();
+		}
+
+		try {
+			em.merge(usuario);
+			em.remove(usuario);
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
---
+++ jpa-prime-login/src/main/Testar.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Date;
+
+import com.framework.db.UsuarioDAO;
+import com.framework.model.Usuario;
+
+public class Testar {
+
+	public static void main(String[] args) {
+		UsuarioDAO dao = new UsuarioDAO();
+		
+		Usuario usuario = new Usuario();
+//		usuario.setId(7);
+		usuario.setNomeUsuario("---teste 2");
+		usuario.setSenha("---234");
+		usuario.setMatricula("-----");
+		usuario.setUltimoAcesso(new Date());
+		
+//		dao.inserirUsuario(usuario);
+		
+
+	}
+
+}
