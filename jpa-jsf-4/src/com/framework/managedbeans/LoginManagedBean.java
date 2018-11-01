8c94b489f028589a3d87dfc9d5bb71a00363bd1f
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-11-01 15:07:02 -0300

    ajustes

--- jpa-jsf-4/src/com/framework/managedbeans/LoginManagedBean.java
+++ jpa-jsf-4/src/com/framework/managedbeans/LoginManagedBean.java
@@ -4,7 +4,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 
 import com.framework.db.UsuarioDAO;
 import com.framework.model.Usuario;
@@ -22,15 +21,14 @@ public class LoginManagedBean {
 		FacesContext context = FacesContext.getCurrentInstance();
 		if (usuario == null) {
 			usuario = new Usuario();
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!", "Erro no Login!"));
-			FacesContext.getCurrentInstance().validationFailed();
-			
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!", "Erro no Login!"));
+			context.validationFailed();
 			context.getExternalContext().getSessionMap().put("logado", false);
 			 
 			return null;
 		} else {
 			context.getExternalContext().getSessionMap().put("logado", true);
+			
 			return "/restrito/listagemUsuarioT.xhtml?faces-redirect=true";
 		}
 	}
