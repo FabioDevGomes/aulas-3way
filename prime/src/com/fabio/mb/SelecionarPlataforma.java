262e55b454006d741bf3f1e16a9ff2ad166ba19c
fabiodevgomes <fabio.dev.gomes@gmail.com>
2018-11-16 16:03:09 -0200

    ajustes

--- prime/src/com/fabio/mb/SelecionarPlataforma.java
+++ prime/src/com/fabio/mb/SelecionarPlataforma.java
@@ -10,52 +10,55 @@ import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class SelecionarPlataforma {
-	   
-    private String option;
-    private List<String> images;
-    
-    @PostConstruct
-    public void init() {
-        images = new ArrayList<String>();
-        //extrair para método
-        for (int i = 1; i <= 5; i++) {
+
+	private String option;
+	private List<String> images;
+
+	@PostConstruct
+	public void init() {
+		images = new ArrayList<String>();
+		carregarJogosXbox();
+		carregarJogosPS4();
+	}
+
+	private void carregarJogosXbox() {
+		for (int i = 1; i <= 5; i++) {
 			images.add("jogoXbox" + i + ".jpg");
 		}
-        //extrair para método
-        for (int i = 1; i <= 5; i++) {
+	}
+
+	private void carregarJogosPS4() {
+		for (int i = 1; i <= 5; i++) {
 			images.add("jogo" + i + ".jpeg");
 		}
-    }
- 
-    public List<String> getImages() {
-        return images;
-    }
-     
-    public String getOption() {
-        return option;
-    }
- 
-    public void setOption(String option) {
-    	FacesContext context = FacesContext.getCurrentInstance();
-    	String mensagem = "";
-    	if(option != null && !option.equals("")) {
-    		images = new ArrayList<String>();
-    		if(option.equals("Xbox One")) {
-    			for (int i = 1; i <= 5; i++) {
-    				images.add("jogoXbox" + i + ".jpg");
-    			}
-    		}else if(option.equals("PS4")) {
-    			for (int i = 1; i <= 5; i++) {
-    				images.add("jogo" + i + ".jpeg");
-    			}
-    		}
-    		mensagem = "Plataforma selecionada com sucesso";
-    	}else {
-    		mensagem = "Todas as plataformas selecionadas";
-    	}
-    	context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successo",  mensagem) );
-        this.option = option;
-    }
+	}
 
+	public List<String> getImages() {
+		return images;
+	}
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		String mensagem = "";
+		if (option != null && !option.equals("")) {
+			images = new ArrayList<String>();
+			// if(option.equalsIgnoreCase("Xbox One")) {
+			// if (option.contains("Xbox One")) {
+			if (option.equals("Xbox One")) {
+				carregarJogosXbox();
+			} else if (option.equals("PS4")) {
+				carregarJogosPS4();
+			}
+			mensagem = "Plataforma selecionada com sucesso";
+		} else {
+			mensagem = "Todas as plataformas selecionadas";
+		}
+		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successo", mensagem));
+		this.option = option;
+	}
 
 }
