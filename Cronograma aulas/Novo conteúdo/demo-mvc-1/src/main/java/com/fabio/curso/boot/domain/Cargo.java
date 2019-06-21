432dfe8e576694e8fdae7ea56844fc3f895c4d1f
f538460 <fabio.dev.gomes@gmail.com>
2019-06-21 14:18:04 -0300

    atualização projeto thymeleaf

---
+++ Cronograma aulas/Novo conteúdo/demo-mvc-1/src/main/java/com/fabio/curso/boot/domain/Cargo.java
@@ -0,0 +1,37 @@
+package com.fabio.curso.boot.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+//transforma essa classe em uma entidade gerenciada pela JPA 
+@Entity
+@Table(name="CARGOS")
+public class Cargo extends AbstractEntity<Long>{
+
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_departamento-fk") //nome da chave estrangeira que teremos na tabela CARGOS
+	private Departamento departamento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+}
