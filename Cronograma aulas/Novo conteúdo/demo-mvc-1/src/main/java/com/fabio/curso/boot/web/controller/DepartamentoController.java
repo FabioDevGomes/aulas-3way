3cef3509decbd917df0ff30f0280e276dc87b32e
f538460 <fabio.dev.gomes@gmail.com>
2019-07-23 13:41:52 -0300

    ajustes work spring

--- Cronograma aulas/Novo conteúdo/demo-mvc-1/src/main/java/com/fabio/curso/boot/web/controller/DepartamentoController.java
+++ Cronograma aulas/Novo conteúdo/demo-mvc-1/src/main/java/com/fabio/curso/boot/web/controller/DepartamentoController.java
@@ -1,20 +1,34 @@
 package com.fabio.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fabio.curso.boot.domain.Departamento;
+import com.fabio.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 }
