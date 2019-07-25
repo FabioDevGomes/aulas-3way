0b7bc6b4c5987bc6fea82c028107f2323a67e990
f538460 <fabio.dev.gomes@gmail.com>
2019-07-25 16:11:21 -0300

    ajustes work

--- Cronograma aulas/Novo conteúdo/demo-mvc-1/src/main/java/com/fabio/curso/boot/web/controller/DepartamentoController.java
+++ Cronograma aulas/Novo conteúdo/demo-mvc-1/src/main/java/com/fabio/curso/boot/web/controller/DepartamentoController.java
@@ -2,6 +2,7 @@ package com.fabio.curso.boot.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,8 @@ public class DepartamentoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
 		return "/departamento/lista";
 	}
 	
