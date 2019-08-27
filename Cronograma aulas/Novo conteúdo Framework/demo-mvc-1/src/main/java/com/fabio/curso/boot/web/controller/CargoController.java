12faafe93db8d21c7ebedd69a689453c7ab0ac58
f538460 <fabio.dev.gomes@gmail.com>
2019-08-27 18:44:30 -0300

    work

--- Cronograma aulas/Novo conteúdo Framework/demo-mvc-1/src/main/java/com/fabio/curso/boot/web/controller/CargoController.java
+++ Cronograma aulas/Novo conteúdo Framework/demo-mvc-1/src/main/java/com/fabio/curso/boot/web/controller/CargoController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -31,7 +33,8 @@ public class CargoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista";
 	}
 	
@@ -40,12 +43,25 @@ public class CargoController {
 		cargoService.salvar(cargo);
 		redirectAttributes.addFlashAttribute("success","Cargo incluído com sucesso.");
 		return "redirect:/cargos/cadastrar";
-		
 	}
 	
 	//essa anotação já coloca o retorno desse método na variável 'departamentos'
 	@ModelAttribute("departamentos")
 	public List<Departamento> listaDepartamentos(){
 		return departamentoService.buscarTodos();
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "/cargo/cadastro";
+	}
+	
+	//esse parâmetro está sendo enviado do formulário .html
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success","Cargo editado com sucesso.");
+		return "redirect:/cargos/cadastrar";
 	}
 }
