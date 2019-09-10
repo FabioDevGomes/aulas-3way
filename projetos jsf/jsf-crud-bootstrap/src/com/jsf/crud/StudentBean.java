67f9553f5ff8c85933780eab5959d5539518dcb4
f538460 <fabio.dev.gomes@gmail.com>
2019-09-10 14:11:30 -0300

    movendo projetos antigos

---
+++ projetos jsf/jsf-crud-bootstrap/src/com/jsf/crud/StudentBean.java
@@ -0,0 +1,114 @@
+package com.jsf.crud;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import com.jsf.crud.db.operations.DatabaseOperation;
+import com.jsf.entidade.Student;
+
+@ManagedBean 
+@RequestScoped
+public class StudentBean {
+
+//	private int id;  
+//	private String name;  
+//	private String email;  
+//	private String password;  
+//	private String gender;  
+//	private String address;
+
+	private ArrayList<Student>studentsListFromDB;
+	private Student Student = new Student();
+
+//	public int getId() {
+//		return id;	
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public String getGender() {
+//		return gender;
+//	}
+//
+//	public void setGender(String gender) {
+//		this.gender = gender;
+//	}
+//
+//	public String getAddress() {
+//		return address;
+//	}
+//
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}  
+	
+	@PostConstruct
+	public void init() {
+		setStudentsListFromDB(DatabaseOperation.getStudentsListFromDB());
+	}
+
+	public ArrayList<Student> studentsList() {
+		return getStudentsListFromDB();
+	}
+	
+	public String saveStudentDetails(Student newStudentObj) {
+		return DatabaseOperation.saveStudentDetailsInDB(newStudentObj);
+	}
+	
+	public String editStudentRecord(int studentId) {
+		return DatabaseOperation.editStudentRecordInDB(studentId);
+	}
+	
+	public String updateStudentDetails(Student updateStudentObj) {
+		return DatabaseOperation.updateStudentDetailsInDB(updateStudentObj);
+	}
+	
+	public String deleteStudentRecord(int studentId) {
+		return DatabaseOperation.deleteStudentRecordInDB(studentId);
+	}
+
+	public Student getStudent() {
+		return Student;
+	}
+
+	public void setStudent(Student student) {
+		Student = student;
+	}
+
+	public ArrayList<Student> getStudentsListFromDB() {
+		return studentsListFromDB;
+	}
+
+	public void setStudentsListFromDB(ArrayList<Student> studentsListFromDB) {
+		this.studentsListFromDB = studentsListFromDB;
+	}
+}
